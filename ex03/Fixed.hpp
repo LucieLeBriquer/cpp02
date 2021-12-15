9f43b9822e7dc3622055e50caeff7f158f052757
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2021-12-15 23:32:28 +0100

    ex03 init

---
+++ ex03/Fixed.hpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/13 17:30:36 by lle-briq          #+#    #+#             */
+/*   Updated: 2021/12/15 23:05:48 by lle-briq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# define CALL_LOG 0
+# include <iostream>
+# include <iomanip>
+# include <cmath>
+
+class Fixed
+{
+	private:
+		int	_pf;
+		static const int _nbFrac = 8;
+
+	public:
+		Fixed(void);
+		Fixed(const Fixed &fixed);
+		Fixed(const int n);
+		Fixed(const float f);
+		~Fixed();
+
+		Fixed	&operator=(const Fixed &fixed);
+		bool	operator>(const Fixed &fixed) const;
+		bool	operator<(const Fixed &fixed) const;
+		bool	operator>=(const Fixed &fixed) const;
+		bool	operator<=(const Fixed &fixed) const;
+		bool	operator==(const Fixed &fixed) const;
+		bool	operator!=(const Fixed &fixed) const;
+
+		Fixed	operator+(const Fixed &fixed) const;
+		Fixed	operator-(const Fixed &fixed) const;
+		Fixed	operator*(const Fixed &fixed) const;
+		Fixed	operator/(const Fixed &fixed) const;
+
+		Fixed	&operator++(void);
+		Fixed	operator++(int);
+		Fixed	&operator--(void);
+		Fixed	operator--(int);
+
+		int		getRawBits(void) const;
+		void	setRawBits(const int raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+
+		static Fixed	&min(Fixed &fixed1, Fixed &fixed2);
+		static Fixed	&max(Fixed &fixed1, Fixed &fixed2);
+		static const Fixed	&min(const Fixed &fixed1, const Fixed &fixed2);
+		static const Fixed	&max(const Fixed &fixed1, const Fixed &fixed2);
+		
+};
+
+std::ostream	&operator<<(std::ostream &o, const Fixed &fixed);
+
+#endif
